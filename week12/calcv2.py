954fc968de0273920c64306eb69e6da67fe7c856
apiphon <74528989+apiphon@users.noreply.github.com>
2024-09-20 10:57:51 +0700

    week12 edit1

---
+++ week12/calcv2.py
@@ -0,0 +1,11 @@
+def main():
+    '''!taskkill /f /im ijudge.exe'''
+    n = str(input())
+    leng = len(n)
+    count = 0
+    them = [21,271,3601,45001,540001,6300001]
+    for i in range(leng,-1,-1):
+        if 
+        count += n[i]*l
+
+main()
